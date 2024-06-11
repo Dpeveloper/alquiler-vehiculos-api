5b98f9441fc68941d27ebe9aab12cddf72e5bbae
Dpeveloper <dqp235497@gmail.com>
2024-06-11 13:32:46 -0500

    mejoras

--- src/main/java/com/d/zsw/alquiler_vehiculos_api/dto/mappers/ReservaMapper.java
+++ src/main/java/com/d/zsw/alquiler_vehiculos_api/dto/mappers/ReservaMapper.java
@@ -1,7 +1,11 @@
 package com.d.zsw.alquiler_vehiculos_api.dto.mappers;
 
+import com.d.zsw.alquiler_vehiculos_api.dto.CarroDto;
+import com.d.zsw.alquiler_vehiculos_api.dto.LocacionDto;
 import com.d.zsw.alquiler_vehiculos_api.dto.ReservaDto;
 import com.d.zsw.alquiler_vehiculos_api.dto.ReservaToSaveDto;
+import com.d.zsw.alquiler_vehiculos_api.entidades.Carro;
+import com.d.zsw.alquiler_vehiculos_api.entidades.PropietarioAlquiler;
 import com.d.zsw.alquiler_vehiculos_api.entidades.Reserva;
 import org.mapstruct.Mapper;
 
@@ -9,7 +13,22 @@ import org.mapstruct.Mapper;
 public interface ReservaMapper {
     Reserva toReserva(ReservaDto reserva);
 
-    ReservaDto toReservaDto(Reserva reserva);
+    default ReservaDto toReservaDto(Reserva reserva){
+        PropietarioAlquiler propietarioAlquiler =
+                new PropietarioAlquiler(reserva.getPropietarioAlquiler().getNombrePropietarioAlquiler(),
+                        reserva.getPropietarioAlquiler().getApellidoPropietarioAlquiler(),
+                        reserva.getPropietarioAlquiler().getTelefonoPropietarioAlquiler());
+        Carro carro = reserva.getCarro();
+
+        CarroDto carroDto = new CarroDto(carro.getId(), carro.getMarca(),carro.getModelo(), carro.getPlaca(), carro.getImagen(), carro.getPrecio(),new LocacionDto(carro.getLocacion().getId(), carro.getLocacion().getNombre()));
+        return new ReservaDto(reserva.getId(),
+             reserva.getFechaInicio(),
+             reserva.getFechaFin(),
+             reserva.getValor(),
+             propietarioAlquiler,
+            reserva.getCarro().getLocacion().getNombre().toString(),
+                carroDto);
+    }
 
     Reserva reservaToSaveDtoToReserva(ReservaToSaveDto reservaToSaveDto);
 }
--- src/main/java/com/d/zsw/alquiler_vehiculos_api/entidades/PropietarioAlquiler.java
+++ src/main/java/com/d/zsw/alquiler_vehiculos_api/entidades/PropietarioAlquiler.java
@@ -1,9 +1,14 @@
 package com.d.zsw.alquiler_vehiculos_api.entidades;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
 public class PropietarioAlquiler {
     private String nombrePropietarioAlquiler;
     private String apellidoPropietarioAlquiler;
