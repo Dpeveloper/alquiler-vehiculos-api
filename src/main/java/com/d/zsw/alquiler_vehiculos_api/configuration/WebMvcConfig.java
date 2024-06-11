035ec1e193e92435772f0fd33dd6a0a1bf10cb8d
Dpeveloper <dqp235497@gmail.com>
2024-06-11 12:49:57 -0500

    locacionesDto

---
+++ src/main/java/com/d/zsw/alquiler_vehiculos_api/configuration/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.d.zsw.alquiler_vehiculos_api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*");
+    }
+}
